754fd8fc93ec0c64440a933e363f55ab22aaeb0f
Hee Suk Shin <heesuk@gmail.com>
2026-04-08 12:06:24 +0900

    Fix the extra gap between rows

--- web/components/ItemGrid.tsx
+++ web/components/ItemGrid.tsx
@@ -32,7 +32,7 @@ export default function ItemGrid({
 
   return (
     <div className="flex-1 flex flex-col">
-      <div className="grid grid-cols-2 gap-2 px-2 flex-1">
+      <div className="grid grid-cols-2 gap-2 px-2">
         {items.map((item) => (
           <ItemCard
             key={item.itemId}
